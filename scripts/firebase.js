b3cf548ed7be846c31ceb5a6f1b21b74aa224e8c
AC1D <moonwatcher445@gmail.com>
2026-02-24 13:32:49 -0500

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -4,21 +4,21 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-storage.js";
 
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCXi-M0wMZT165T9FHCK8Dw-WJK_I1MuDg",
   authDomain: "gitchat-backend.firebaseapp.com",
   projectId: "gitchat-backend",
-  storageBucket: "gitchat-backend.firebasestorage.app",
+  storageBucket: "gitchat-backend.appspot.com",
   messagingSenderId: "385257284178",
   appId: "1:385257284178:web:20d7ce32ba1b1ed554f58b"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 
-// Auto anonymous login
-signInAnonymously(auth)
-  .then(() => console.log("Anonymous login ✅"))
-  .catch(err => console.error("Anonymous login failed:", err));
+// Sign in anonymously
+signInAnonymously(auth).then(() => console.log("Signed in anonymously ✅"));
